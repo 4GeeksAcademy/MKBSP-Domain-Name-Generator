e97f693749816828a8d74e5f20f1698d439b6aa9
Mads Schmidt Petersen <MadsTrundte@MacBook-Pro-7.local>
2023-11-19 21:04:38 -0500

    I added another variable array to the mix, think it is better now

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //Arrays with combis of Pronouns, Adjectives, Nouns, and TLD
+  let arrayPronoun = ["the", "our", "your"];
+  let arrayAdj = ["bold", "amazing", "infinite"];
+  let arrayNoun = ["soccer", "venture", "horizon"];
+  let arrayTLD = [".org", ".net", ".com"];
+
+  //Nested Loops printing the domain name combinations to the Console.
+  for (var i = 0; i < arrayPronoun.length; i++) {
+    for (var a = 0; a < arrayAdj.length; a++) {
+      for (var b = 0; b < arrayNoun.length; b++) {
+        for (var c = 0; c < arrayTLD.length; c++) {
+          console.log(
+            arrayPronoun[i] + arrayAdj[a] + arrayNoun[b] + arrayTLD[c]
+          );
+        }
+      }
+    }
+  }
 };
